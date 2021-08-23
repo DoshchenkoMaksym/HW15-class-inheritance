46f7b433f0e648656684eda04b558073e86f6566
Maksym Doshchenko <doschenko54@gmail.com>
2021-08-23 21:13:57 +0300

    fixed

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
 let clock = document.querySelector('.clock');
 
 function Clock(clock) {
-    this.isFullFormat = true;
-    clock.addEventListener('click', () => { this.switchScreen() });
-    this.switchScreen = function () {
-        this.toggleFormat();
-    };
-    this.toggleFormat = function () {
-        this.isFullFormat = !this.isFullFormat;
-    }
-
 }
 
 Clock.prototype.render = function () {
-    if (this.isFullFormat) {
-        fullTime.render();
-        console.log('1');
-    } else {
-        shortTime.render();
-        console.log('2')
-    }
-
+    let data = new Date();
+    let hours = data.getHours();
+    clock.innerHTML = `${hours}`;
 };
-
+Clock.prototype.interval = function () {
+    let ourInterval = setInterval(() => this.render(), 250);
+    return ourInterval;
+}
 function FullFormat(clock) {
     Clock.call(this, clock)
     this.render = function () {
@@ -44,7 +33,6 @@ function ShortFormat(clock) {
         let data = new Date();
         let hours = data.getHours();
         let minutes = data.getMinutes();
-        let seconds = data.getSeconds();
         clock.innerHTML = `${hours}:${minutes}`;
     };
 };
@@ -56,5 +44,28 @@ ShortFormat.prototype.constructor = ShortFormat;
 
 let time = new Clock(clock);
 let fullTime = new FullFormat(clock);
-let shortTime = new ShortFormat(clock)
-setInterval(() => time.render(), 250);
+let shortTime = new ShortFormat(clock);
+let isFullFormat = true;
+let fullInterval;
+let shortInterval;
+
+function toggleFormat() {
+    return isFullFormat = !isFullFormat;
+};
+
+clock.addEventListener('click', () => {
+    
+    if (toggleFormat()) {
+        clearInterval(mainTime);
+        clearInterval(shortInterval);
+        fullInterval = fullTime.interval();
+        console.log('1');
+    } else {
+        clearInterval(mainTime);
+        clearInterval(fullInterval);
+        shortInterval = shortTime.interval();
+        console.log('2');
+    }
+});
+
+let mainTime = time.interval();
