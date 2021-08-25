55c0077c6d9026ecdb0fdf6bd9438bd56c2b233b
Maksym Doshchenko <doschenko54@gmail.com>
2021-08-25 11:16:49 +0300

    fixed2

--- main.js
+++ main.js
@@ -1,50 +1,59 @@
 let clock = document.querySelector('.clock');
 
 function Clock(clock) {
+    this.elem = clock;
 }
 
 Clock.prototype.render = function () {
     let data = new Date();
     let hours = data.getHours();
-    clock.innerHTML = `${hours}`;
+    let minutes = data.getMinutes();
+    let seconds = data.getSeconds();
+    this.elem.innerHTML = `${hours}:${minutes}:${seconds}`;
 };
 Clock.prototype.interval = function () {
     let ourInterval = setInterval(() => this.render(), 250);
     return ourInterval;
 }
-function FullFormat(clock) {
-    Clock.call(this, clock)
-    this.render = function () {
-        let data = new Date();
-        let hours = data.getHours();
-        let minutes = data.getMinutes();
-        let seconds = data.getSeconds();
-        clock.innerHTML = `${hours}:${minutes}:${seconds}`;
-    };
+
+function FullFormat() {
+    Clock.call(this, clock);
 };
 
 FullFormat.prototype = Object.create(Clock.prototype);
+
+FullFormat.prototype.render = function () {
+    let data = new Date();
+    let hours = data.getHours();
+    let minutes = data.getMinutes();
+    let seconds = data.getSeconds();
+    this.elem.innerHTML = `${hours}:${minutes}:${seconds}`;
+};
+
 FullFormat.prototype.constructor = FullFormat;
 
 
-function ShortFormat(clock) {
-    Clock.call(this, clock)
-    this.render = function () {
-        let data = new Date();
-        let hours = data.getHours();
-        let minutes = data.getMinutes();
-        clock.innerHTML = `${hours}:${minutes}`;
-    };
+
+function ShortFormat() {
+    Clock.call(this, clock);
 };
 
 ShortFormat.prototype = Object.create(Clock.prototype);
+
+ShortFormat.prototype.render = function () {
+    let data = new Date();
+    let hours = data.getHours();
+    let minutes = data.getMinutes();
+    this.elem.innerHTML = `${hours}:${minutes}`;
+};
+
 ShortFormat.prototype.constructor = ShortFormat;
 
 
 
 let time = new Clock(clock);
-let fullTime = new FullFormat(clock);
-let shortTime = new ShortFormat(clock);
+let fullTime = new FullFormat();
+let shortTime = new ShortFormat();
 let isFullFormat = true;
 let fullInterval;
 let shortInterval;
@@ -54,18 +63,17 @@ function toggleFormat() {
 };
 
 clock.addEventListener('click', () => {
-    
+
     if (toggleFormat()) {
         clearInterval(mainTime);
         clearInterval(shortInterval);
         fullInterval = fullTime.interval();
-        console.log('1');
     } else {
         clearInterval(mainTime);
         clearInterval(fullInterval);
         shortInterval = shortTime.interval();
-        console.log('2');
     }
+
 });
 
 let mainTime = time.interval();
